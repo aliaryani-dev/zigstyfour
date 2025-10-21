8ccfad647e21574aa1060d6c69cf0ec82fc3ab78
Ali Aryani <lenesis@proton.me>
2025-10-21 14:10:03 +0330

    declare stdout

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+var stdout_buffer:[1024]u8 = undefined;
+var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+const stdout = &stdout_writer.interface;
 
 const Base64 = struct {
     _table: *const[64]u8,
