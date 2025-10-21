d5817ba67523380bfb23f9622c9f6f0eaf74d695
Ali Aryani <lenesis@proton.me>
2025-10-21 14:23:28 +0330

    move code to base64.zig

---
+++ src/base64.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+const Base64 = struct {
+    _table: *const[64]u8,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const number_symbol = "0123456789+/";
+        return Base64 {
+            ._table = upper ++ lower ++ number_symbol,
+        };
+    }
+
+    pub fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+};
+
+fn _calc_encode_length(input: []const u8) !usize {
+    if (input < 3) {
+        return 4;
+    }
+    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+    return n_groups * 4;
+}
+
+fn _calc_decode_length(input: []const u8) !usize {
+    if (input.len < 4) {
+        return 3;
+    }
+
+    const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
+    var multiple_groups: usize = n_groups * 3;
+    var i: usize = input.len - 1;
+    while (i > 0) : (i -= 1) {
+        if (input[i] == '=') {
+            multiple_groups -= 1;
+        } else {
+            break;
+        }
+    }
+
+    return multiple_groups;
+}
