54e87dc2bdf4ce1d204ab17354fce2a3dbd09cc2
Ali Aryani <lenesis@proton.me>
2025-10-21 15:19:36 +0330

    declare and write logic for decode()

--- src/base64.zig
+++ src/base64.zig
@@ -96,3 +96,34 @@ fn _char_index(self:Base64, char:u8) u8 {
     }
     return index;
 }
+
+pub fn decode(self: Base64, allocator:std.mem.Allocator, input:[]const u8) ![]u8 {
+    if (input.len == 0) {
+        return "";
+    }
+
+    const n_output = try _calc_decode_length(input);
+    var output = try allocator.alloc(u8, n_output);
+    var count: u8 = 0;
+    var iout: u64 = 0;
+    var buf = [4]u8{ 0, 0, 0, 0 };
+
+    for (0..input.len) |i| {
+        buf[count] = self._char_index(input[i]);
+        count += 1;
+        if (count == 4) {
+            output[iout] = (buf[0] << 2) + (buf[1] >> 4);
+            if (buf[2] != 64) {
+                output[iout + 1] = (buf[1] << 4) + (buf[2] >> 2);
+            }
+            if (buf[3] != 64) {
+                output[iout + 2] = (buf[2] << 6) + buf[3];
+            }
+            iout += 3;
+            count = 0;
+        }
+    }
+
+    return output;
+}
+
