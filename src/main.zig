840d1602cddf83f64226fe2a0143fde452ee5f24
Ali Aryani <lenesis@proton.me>
2025-10-21 11:58:42 +0330

    encode/decode length

--- src/main.zig
+++ src/main.zig
@@ -16,3 +16,30 @@ const Base64 = struct {
         return self._table[index];
     }
 };
+
+fn _calc_encode_length(input: []const u8) !usize {
+    if (input < 3) {
+        return 4;
+    }
+    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+    return n_groups * 4;
+}
+
+fn _calc_decode_length(input: []const u8) !usize {
+    if (input.len < 4) {
+        return 3;
+    }
+
+    const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
+    var multiple_groups: usize = n_groups * 3;
+    var i: usize = input.len - 1;
+    while (i > 0) : (i -= 1) {
+        if (input[i] == '=') {
+            multiple_groups -= 1;
+        } else {
+            break;
+        }
+    }
+
+    return multiple_groups;
+}
